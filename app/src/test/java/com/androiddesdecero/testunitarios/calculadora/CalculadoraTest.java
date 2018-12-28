e96f20d7a0362848721cbc75f32ad1d90f873a3d
Alberto <info@androiddesdecero.com>
2018-12-28 12:21:24 +0100

    10

--- app/src/test/java/com/androiddesdecero/testunitarios/calculadora/CalculadoraTest.java
+++ app/src/test/java/com/androiddesdecero/testunitarios/calculadora/CalculadoraTest.java
@@ -1,5 +1,6 @@
 package com.androiddesdecero.testunitarios.calculadora;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,11 +13,30 @@ import static org.junit.Assert.*;
 public class CalculadoraTest {
 
     public Calculadora calculadoraNull;
+    public Calculadora calculadora;
+
+    @Before
+    public void setUp(){
+        calculadora = new Calculadora();
+        System.out.println("Se ha ejecutado el test setUp()");
+    }
+
+    @After
+    public void tearDown(){
+        calculadora = null;
+        System.out.println("Se ha ejecutado el test tearDown()");
+    }
+
+    @Test
+    public void caculadoraNotNullTest(){
+        Assert.assertNotNull("Calculadora debe ser not null", calculadora);
+        System.out.println("Se ha ejecutado el test caculadoraNotNullTest()");
+    }
 
     @Test
     public void caculadoraNullTest(){
         Assert.assertNull("Calculadora debe ser null", calculadoraNull);
-        System.out.println("Se ha ejecutado el test caculadoraNullTest");
+        System.out.println("Se ha ejecutado el test caculadoraNullTest()");
     }
 
     /*
