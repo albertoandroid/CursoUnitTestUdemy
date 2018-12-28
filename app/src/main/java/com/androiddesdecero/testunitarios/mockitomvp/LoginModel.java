3492df032647c0d01a7fa054ce673d61634c33d0
Alberto <info@androiddesdecero.com>
2018-12-28 21:22:47 +0100

    04

--- app/src/main/java/com/androiddesdecero/testunitarios/mockitomvp/LoginModel.java
+++ app/src/main/java/com/androiddesdecero/testunitarios/mockitomvp/LoginModel.java
@@ -4,5 +4,20 @@ package com.androiddesdecero.testunitarios.mockitomvp;
  * Created by albertopalomarrobledo on 28/12/18.
  */
 
-public class LoginModel {
+public class LoginModel implements Login.Model {
+
+    private Login.Presenter presenter;
+
+    public LoginModel(Login.Presenter presenter){
+        this.presenter = presenter;
+    }
+
+    @Override
+    public void validaUser(String user, String password) {
+        if(user.equals("alberto") && password.equals("1234")){
+            presenter.usuarioValido();
+        }else {
+            presenter.error();
+        }
+    }
 }
--- app/src/main/java/com/androiddesdecero/testunitarios/mockitomvp/LoginPresenter.java
+++ app/src/main/java/com/androiddesdecero/testunitarios/mockitomvp/LoginPresenter.java
@@ -4,5 +4,34 @@ package com.androiddesdecero.testunitarios.mockitomvp;
  * Created by albertopalomarrobledo on 28/12/18.
  */
 
-public class LoginPresenter {
+public class LoginPresenter implements Login.Presenter {
+
+    private Login.View view;
+    private Login.Model model;
+
+    public LoginPresenter(Login.View view){
+        this.view = view;
+        model = new LoginModel(this);
+    }
+
+    @Override
+    public void validadUser(String user, String password) {
+        if(view != null){
+            model.validaUser(user, password);
+        }
+    }
+
+    @Override
+    public void usuarioValido() {
+        if(view != null){
+            view.usuarioValido();
+        }
+    }
+
+    @Override
+    public void error() {
+        if(view != null){
+            view.error();
+        }
+    }
 }
