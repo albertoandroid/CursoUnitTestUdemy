707523e6bb753fcc4fee3b7c40f9f6b9f60b3e1a
Alberto <info@androiddesdecero.com>
2018-12-28 22:12:20 +0100

    08

---
+++ app/src/test/java/com/androiddesdecero/testunitarios/mockitomvp/LoginPresenterTest.java
@@ -0,0 +1,14 @@
+package com.androiddesdecero.testunitarios.mockitomvp;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by albertopalomarrobledo on 28/12/18.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class LoginPresenterTest {
+
+}
