38a519a2c6800e2f3682f47d628e77a0f1efd970
Alberto <info@androiddesdecero.com>
2018-12-28 19:38:06 +0100

    21

--- app/src/test/java/com/androiddesdecero/testunitarios/calculadora/CalculadoraParametrizadaTest.java
+++ app/src/test/java/com/androiddesdecero/testunitarios/calculadora/CalculadoraParametrizadaTest.java
@@ -1,7 +1,14 @@
 package com.androiddesdecero.testunitarios.calculadora;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by albertopalomarrobledo on 28/12/18.
@@ -14,4 +21,47 @@ public class CalculadoraParametrizadaTest {
     private int numero2;
     private int resultadoExperado;
     private Calculadora calculadora;
+
+    /*
+    Ejemplo en nuestra división queremos hacer 5 pruebas
+    Positivo / Positivo = Positivo
+    Positivo / Negativo = Negativo
+    Negativo / Positivo = Negativo
+    Negativo / Negativo = Positivo
+    Positivo / 0 = Excepción
+
+    Método a Probar                 |      Entrada      |       Salida Experarada
+    dividir(int a, int b)           |a = 6, b=2         |3
+    dividir(int a, int b)           |a = 6, b=-2        |-3
+    dividir(int a, int b)           |a = -6, b=2        |-3
+    dividir(int a, int b)           |a = -6, b=-2       |3
+    dividir(int a, int b)           |a = -6, b=0        |Excepción
+     */
+
+    @Parameterized.Parameters
+    public static Iterable<Object[]> obtenerDatosPruebaTest(){
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[]{6,2,-3});
+        list.add(new Object[]{6,-2,-3});
+        list.add(new Object[]{-6,2,-3});
+        list.add(new Object[]{-6,-2,3});
+        list.add(new Object[]{6,0,3});
+        return list;
+    }
+
+    public CalculadoraParametrizadaTest(int a, int b, int c){
+        numero1 = a;
+        numero2 = b;
+        resultadoExperado = c;
+    }
+
+    @Before
+    public void setUp(){
+        calculadora = new Calculadora();
+    }
+
+    @Test
+    public void dividirTest(){
+        Assert.assertEquals(resultadoExperado, calculadora.dividir(numero1, numero2));
+    }
 }
