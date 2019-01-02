0b66a948204198889301470f0bda77c8d8d2491c
Alberto <info@androiddesdecero.com>
2019-01-02 18:24:34 +0100

    04

--- app/src/test/java/com/androiddesdecero/testunitarios/tdd/FizzBuzzTest.java
+++ app/src/test/java/com/androiddesdecero/testunitarios/tdd/FizzBuzzTest.java
@@ -7,4 +7,30 @@ import static org.junit.Assert.*;
  */
 public class FizzBuzzTest {
 
+
+    /*
+     //METODO////////////////////////////////////////ESPECIFICACIOM//////////////////////////////////////////////////////////////////////////////
+    ------------------------------------------------------------------------------------------------------------------------
+    String of(int numero)    |1.- Si es divisible por 3 entonces devolver "Fizz"
+                             |2.- Si es divisible por 5 entonces devolver "Buzz"
+                             |3.- Si es divisible por 3 y 5 entonces devolver "FizzBuzz"
+                             |4.- Si nada de lo de arriba ocurre devolver el número en String
+
+
+    Método a Probar                           |   Entrada       |  Salida Experarada
+    of(int numero)                            |numero = 0       |0
+    of(int numero)                            |numero = 1       |1
+    of(int numero)                            |numero = 2       |2
+    of(int numero)                            |numero = 3       |Fizz
+    of(int numero)                            |numero = 4       |4
+    of(int numero)                            |numero = 5       |Buzz
+    of(int numero)                            |numero = 6       |Fizz
+    of(int numero)                            |numero = 7       |7
+    of(int numero)                            |numero = 8       |8
+    of(int numero)                            |numero = 9       |Fizz
+    of(int numero)                            |numero = 10      |Buzz
+    of(int numero)                            |numero = 15      |FizzBuzz
+    of(int numero)                            |numero = 100     |Buzz
+    -----------------------------------------------------------------------------------------
+    */
 }
